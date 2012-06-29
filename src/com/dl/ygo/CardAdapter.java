d8c6a23ced515899370a8ad340e450f8ee0e5aab
wangdl2005 <wangdl1989@yahoo.com.cn>
2012-06-29 18:47:47 +0800

    cardsView details add

--- src/com/dl/ygo/CardAdapter.java
+++ src/com/dl/ygo/CardAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CardAdapter extends BaseAdapter{
 
@@ -37,20 +39,28 @@ public class CardAdapter extends BaseAdapter{
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		ImageView imgView = null;
-		if(convertView == null){
-			imgView = new ImageView(context);
-			imgView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT
-					,GridView.LayoutParams.WRAP_CONTENT));
-			imgView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-			imgView.setPadding(8, 8, 8, 8);
-		}
-		else{
-			imgView = (ImageView) convertView;
-		}
+//		ImageView imgView = null;
+//		if(convertView == null){
+//			imgView = new ImageView(context);
+//			imgView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT
+//					,GridView.LayoutParams.WRAP_CONTENT));
+//			imgView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//			imgView.setPadding(8, 8, 8, 8);
+//		}
+//		else{
+//			imgView = (ImageView) convertView;
+//		}
+//		imgView.setImageBitmap(cards.getCards().get(position).getCardPic());
+//		//imgView.setImageBitmap(BitmapFactory.decodeResource(context.getResources(),R.drawable.ic_m1));
+//		return imgView;
+		View view = View.inflate(context, R.layout.grid_item, null);
+		LinearLayout lLayout = (LinearLayout) view.findViewById(R.id.grid_linear);
+		ImageView imgView = (ImageView) lLayout.findViewById(R.id.image);
+		TextView txtView = (TextView) lLayout.findViewById(R.id.title);
+		imgView.setPadding(8, 8, 8, 8);
 		imgView.setImageBitmap(cards.getCards().get(position).getCardPic());
-		//imgView.setImageBitmap(BitmapFactory.decodeResource(context.getResources(),R.drawable.ic_m1));
-		return imgView;
+		txtView.setText("[" + position+"]");
+		return lLayout;
 	}
 
 }
